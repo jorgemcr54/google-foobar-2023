81943178e3f1a1ac5b115841cecc4bbcb58f1052
Jorge Mario Cardona Ramírez <jorgemcr54@gmail.com>
2023-08-17 17:03:17 -0500

    add 3 challenge

--- gearing-up-for-destruction/src/Main.java
+++ gearing-up-for-destruction/src/Main.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -9,28 +7,28 @@ public class Main {
     }
 
     public static int[] solution(int[] pegs) {
-        List<Integer> ratios = new ArrayList<>();
-        for (int i = 1; i < pegs[1]-pegs[0]; i++) {
-            ratios.add(i);
-            for (int j = 1; j < pegs.length; j++) {
-
+        int[] res = {-1,-1};
+        double r = 0;
+        for (int n = 1; n <= 10000; n++) {
+            for (int d = n-1; d >= 1; d--) {
+                int n1 = n;
+                for (int i = 0; i < pegs.length-1; i++) {
+                    if ((pegs[i]+(n1/d)>=pegs[i+1]-1)){
+                        break;
+                    }else {
+                        n1= (pegs[i+1]*d) - ((pegs[i]*d) + (n1));
+                        r= ((double) n / (double) d)/((double) n1 / (double) d);
+                        if ((i == pegs.length-2)&&(r==2)){
+                            res[0] =n;
+                            res[1]= d;
+                            System.out.println(res[0] + " " +  res[1]);
+                            return res;
+                        }
+                    }
+                }
             }
-            ratios.forEach(System.out::println);
-            ratios.clear();
-            System.out.println("otro");
         }
-
-
-
-        int[] a= {1,1};
-        return a;
-    }
-
-    public static boolean finalRpm(int[] list){
-        double v=1;
-        for (int i = 0; i < list.length-1; i++) {
-            v= list[i]*v/(list[i+1]);
-        }
-        return (v==2);
+        System.out.println(res[0] + " " +  res[1]);
+        return res;
     }
 }
