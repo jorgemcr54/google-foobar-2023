26950ee4c1115997527c97d244fd6fca84ab32b9
Jorge Mario <jorgemcr54@icloud.com>
2023-08-24 07:38:27 -0500

    challenge 5

--- the-grandest-staircase-of-them-all/src/Main.java
+++ the-grandest-staircase-of-them-all/src/Main.java
@@ -1,8 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(solution(200));
+        System.out.println(solution(3));
     }
     public static int solution(int n) {
-        return 2;
+        recursiveSearch(2,0,)
+    }
+
+    public static boolean isValid(List<Integer> staircase, int totalBricks){
+        return staircase.stream()
+                .mapToInt(Integer::intValue)
+                .sum() == totalBricks;
+    }
+
+    public static int recursiveSearch(int steps, int cont, int pos, List<Integer> staircase ,int total){
+        if (pos>steps)
+        {
+            return cont;
+        }
+        for (int i = 1; i <= total ; i++) {
+            staircase.set(pos,staircase.get(pos)+1);
+            if (isValid(staircase,total)){
+                cont += 1;
+                staircase.forEach(System.out::println);
+            }
+            recursiveSearch(steps,cont,pos+1,staircase,total);
+        }
+
     }
 }
