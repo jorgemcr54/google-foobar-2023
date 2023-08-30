24eed5f384b25f4626c3d23198d6112872cc6f18
Jorge Mario Cardona Ramírez <jorgemcr54@gmail.com>
2023-08-29 22:21:11 -0500

    find valid pairs

--- distract-the-trainers/solution.py
+++ distract-the-trainers/solution.py
@@ -1,31 +1,24 @@
 def solution(banana_list):
-    print(validate_pair(13,19))
+    banana_list.sort(reverse=True)
+    potencias_dos = [2 ** n for n in range(1, 32)]
+    potencias_dos_mas_uno = [x - 1 for x in potencias_dos[:-2]]
+    lista_parejas = []
+    for i in range(len(banana_list)):
+        for j in range(i + 1, len(banana_list)):
+            if validate_pair(banana_list[i],banana_list[j],potencias_dos,potencias_dos_mas_uno):
+                lista_parejas.append([i,j])
+    print(lista_parejas)
+    print(len(lista_parejas))
 
-def validate_pair(a, b):
-    list_pairs = []
-    list_pairs.append([a,b])
-    print(str(a) + ' ' + str(b))
-    cont = 0
-    while(True):
-        cont=cont+1
-        print(cont) 
-        print(str(a) + ' ' + str(b))
-        if a>b:
-            a=a-b
-            b=b*2
-        elif a<b:
-            b=b-a
-            a=a*2
-        elif a==b:
-            return False
-        if [a,b] in list_pairs:
-            return True
-        else:
-            list_pairs.append([a,b])
-            list_pairs.append([b,a])
-     
+
+def validate_pair(a, b, lista_potencias,lista_potencias_mas_uno):
+    if  a+b in lista_potencias or a/b in lista_potencias_mas_uno:
+        return False
+    else:
+        return True
 
     
 
-print(validate_pair(10737,432648))
+#print(validate_pair(10737,432648))
+solution([4,365,21,243,65,7,8])
 
